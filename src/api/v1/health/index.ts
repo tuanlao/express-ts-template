6d177d383091fb0a5e4c424f633be2c31fdbf413
Tuan Lao Van <laotuan@telio.vn>
2022-03-08 11:16:38 +0700

    fix: add async router handler

--- src/api/v1/health/index.ts
+++ src/api/v1/health/index.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import { asyncRouteHandler } from 'middlewares';
 import { checkHealth } from './controller';
 
 const router = Router();
 
-router.get('/', checkHealth);
+router.get('/', asyncRouteHandler, checkHealth);
 
 export default router;
