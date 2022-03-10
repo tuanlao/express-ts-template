881c44690fe7e71c9df93739dd7fae7af606edcc
Tuan Lao Van <laotuan@telio.vn>
2022-03-10 11:15:40 +0700

    refactor: move health to global endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import v1Routers from './v1';
+import healthRouter from './health';
 
 const router = Router();
 
 router.use('/v1', v1Routers);
+router.use('/health', healthRouter);
 
 export default router;
